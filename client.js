1e53f58d6274d08ac67116a0bb18ea8d274c1906
Ben Olayinka <ben.olayinka@gmail.com>
2019-11-05 16:18:39 +0100

    exec

--- client.js
+++ client.js
@@ -4,20 +4,32 @@ const roverIp = process.env.ROVER_IP || '192.168.1.221'
 
 if(process.env.WS)
 {
-  const { spawn } = require('child_process');
-  const ls = spawn('gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=640, height=480, framerate=(fraction)10/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=benolayinka.com port=8004');
+  const { exec } = require('child_process');
+  exec('gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=640, height=480, framerate=(fraction)10/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=benolayinka.com port=8004', (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return;
+    }
 
-  ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
   });
 
-  ls.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+  // const {spawn} = require('child_process');
+  // const ls = spawn('gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=640, height=480, framerate=(fraction)10/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=benolayinka.com port=8004');
 
-  ls.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+  // ls.stdout.on('data', (data) => {
+  //   console.log(`stdout: ${data}`);
+  // });
+
+  // ls.stderr.on('data', (data) => {
+  //   console.error(`stderr: ${data}`);
+  // });
+
+  // ls.on('close', (code) => {
+  //   console.log(`child process exited with code ${code}`);
+  // });
 }
 
 const path = 'wss://benolayinka.com/ws'
