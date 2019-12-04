9c6ea2847620b7ad3863e4c66b4d03fc4f4277b9
ben <ben.olayinka@gmail.com>
2019-12-04 19:56:05 +0100

    cleanup

--- gstreamer.sh
+++ gstreamer.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Load up .env
+set -o allexport
+[[ -f .env ]] && source .env
+set +o allexport
+
+echo "Streaming to.."
+echo "APP_HOSTNAME = "
+echo "$APP_HOSTNAME"
+echo "PORT = "
+echo "$VIDEO_PORT"
 gst-launch-1.0 -v v4l2src device=/dev/video0 ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=$APP_HOSTNAME port=$VIDEO_PORT
-#gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=1280, height=720, framerate=(fraction)10/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=benolayinka.com port=8004
