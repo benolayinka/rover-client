5855400744889d34a15026b9f381c2f93c9a0eb5
ben <ben.olayinka@gmail.com>
2019-12-04 19:12:02 +0100

    env

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ set -o allexport
 [[ -f .env ]] && source .env
 set +o allexport
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=1280, height=720, framerate=(fraction)10/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=benolayinka.com port=$VIDEO_PORT
+gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=$VIDEO_WIDTH, height=$VIDEO_HEIGHT, framerate=(fraction)10/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=benolayinka.com port=$VIDEO_PORT
