fe43e5fa4f2986f9d82f791da4a21a24480772a2
ben <ben.olayinka@gmail.com>
2019-12-04 22:22:21 +0100

    video res stuff

--- gstreamer.sh
+++ gstreamer.sh
@@ -11,4 +11,4 @@ echo "$APP_HOSTNAME"
 echo "PORT = "
 echo "$VIDEO_PORT"
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=$VIDEO_WIDTH, height=$VIDEO_HEIGHT, framerate=(fraction) $VIDEO_FPS /1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=$APP_HOSTNAME port=$VIDEO_PORT
+gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, format=YUY2, width=$VIDEO_WIDTH, height=$VIDEO_HEIGHT, framerate=(fraction)$VIDEO_FPS/1" ! videoconvert ! queue ! omxh264enc ! queue ! rtph264pay pt=96 config-interval=1 ! udpsink host=$APP_HOSTNAME port=$VIDEO_PORT
