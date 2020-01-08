9c25bc0c3c046522eeed6732e53f7d8287086cba
ben <ben.olayinka@gmail.com>
2020-01-08 19:04:55 +0100

    video rotation

--- gstreamer.sh
+++ gstreamer.sh
@@ -13,10 +13,9 @@ echo "$VIDEO_PORT"
 
 if [ "$VIDEO_ROTATION" -gt "0" ]
 	then v4l2-ctl --set-ctrl=rotate=$VIDEO_ROTATION
+fi
 
-gst-launch-1.0 -v v4l2src ! video/x-h264,width=$VIDEO_WIDTH,height=$VIDEO_HEIGHT,framerate=$VIDEO_FPS/1, profile=baseline ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=$APP_HOSTNAME port=$VIDEO_PORT
-
-# gst-launch-1.0 -v v4l2src \
-# ! video/x-h264,width=$VIDEO_WIDTH,height=$VIDEO_HEIGHT,framerate=$VIDEO_FPS/1, profile=baseline \
-# ! h264parse ! rtph264pay config-interval=1 pt=96 \
-# ! udpsink host=$APP_HOSTNAME port=$VIDEO_PORT
+gst-launch-1.0 -v v4l2src \
+! video/x-h264,width=$VIDEO_WIDTH,height=$VIDEO_HEIGHT,framerate=$VIDEO_FPS/1, profile=baseline \
+! h264parse ! rtph264pay config-interval=1 pt=96 \
+! udpsink host=$APP_HOSTNAME port=$VIDEO_PORT
