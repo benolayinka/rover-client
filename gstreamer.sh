d88ce6af961bc7cdb81f5f8514f6d07debd139bd
ben <ben.olayinka@gmail.com>
2020-10-09 21:18:02 +0200

    h264 baseline

--- gstreamer.sh
+++ gstreamer.sh
@@ -15,6 +15,7 @@ if [ "$VIDEO_ROTATION" -gt "0" ]
 	then v4l2-ctl --set-ctrl=rotate=$VIDEO_ROTATION
 fi
 
+#tested framerate and resolution, they are set by gst anyway
 #v4l2-ctl --set-fmt-video=width=$VIDEO_WIDTH,height=$VIDEO_HEIGHT,pixelformat=4
 #v4l2-ctl --overlay=1
 #v4l2-ctl -p $VIDEO_FPS
@@ -22,7 +23,8 @@ v4l2-ctl --set-ctrl scene_mode=8 #night mode
 v4l2-ctl --set-ctrl=video_bitrate=4000000 #or whatever
 v4l2-ctl --set-ctrl=brightness=55
 
+#h264parse seems to affect stream stability in janus. also sets profile=high
 gst-launch-1.0 -v v4l2src \
-! video/x-h264,width=$VIDEO_WIDTH,height=$VIDEO_HEIGHT,framerate=$VIDEO_FPS/1, profile=baseline \
+! video/x-h264,width=$VIDEO_WIDTH,height=$VIDEO_HEIGHT,framerate=$VIDEO_FPS/1, profile=high \
 ! h264parse ! rtph264pay config-interval=1 pt=96 \
 ! udpsink sync=false host=$APP_HOSTNAME port=$VIDEO_PORT
